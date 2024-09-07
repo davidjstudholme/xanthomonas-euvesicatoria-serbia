bb9ebe9460ec05666e2818fe9432b4fdf9f03b75
David Studholme <d.j.studholme@exeter.ac.uk>
2024-09-07 11:04:42 +0000

    Added gzip step

--- 05_unicycler_assemblies/commands.sh
+++ 05_unicycler_assemblies/commands.sh
@@ -1,6 +1,9 @@
 ### Download all the sequence reads from the SRA database using NCBI's SRA Toolkit
 fasterq-dump --split-files SRR24958750 SRR24958751 SRR24958752
 
+### Compress the downloaded FASTQ files
+for i in *.fastq; do echo $i; done
+
 ### Perform TrimGalore on each dataset
 for i in  SRR24958750 SRR24958751 SRR24958752; do echo $i; trim_galore -q 30 --paired  $i*_1.fastq.gz $i*_2.fastq.gz; done
 
