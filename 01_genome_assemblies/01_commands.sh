49539a6c1fbfd177d15be0308202d2bfbbb18f72
David Studholme <d.j.studholme@exeter.ac.uk>
2025-08-13 18:20:45 +0000

    Ran PhaME

--- 01_genome_assemblies/01_commands.sh
+++ 01_genome_assemblies/01_commands.sh
@@ -1,9 +1,9 @@
 ### Download NCBI's datasets utility:
-curl -o datasets 'https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/v1/linux-amd64/datasets'
+curl -o datasets 'https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/LATEST/linux-amd64/datasets'
 chmod u+x datasets
 
 ### Use NCBI's datasets utility to download the genome sequences, unzip them and make symlinks in curren directory:
-./datasets download genome accession --inputfile xanthomonas_assm_accs.txt  --exclude-gff3 --exclude-protein --exclude-rna --exclude-genomic-cds --filename xanthomonas_genome_assemblies.zip
+./datasets download genome accession --inputfile xanthomonas_assm_accs.txt --include genome --filename xanthomonas_genome_assemblies.zip
 unzip xanthomonas_genome_assemblies.zip
 ln -s ncbi_dataset/data/GCA_*/GCA_*.fna .
 
