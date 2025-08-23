b7bc3a67bc93febe45d863d2dbfc995cda484cd4
David Studholme <d.j.studholme@exeter.ac.uk>
2025-08-23 07:14:36 +0000

    Added more strains into phylogenomics

--- 03_workdir/commands.sh
+++ 03_workdir/commands.sh
@@ -1,3 +1,4 @@
 ln -s ../01_genome_assemblies/*.contig .
 
+rm ATCC_11633.contig 
 
---
+++ 12_snp_calling/commands.sh
@@ -0,0 +1,23 @@
+ln -s ../01_genome_assemblies/Xe173.fasta .
+ln -s ../01_genome_assemblies/85-10.fasta .
+ln -s ../01_genome_assemblies/LMG_930.fasta .
+ln -s ../01_genome_assemblies/X22.fasta .
+ln -s ../01_genome_assemblies/X13.fasta .
+ln -s ../01_genome_assemblies/X31.fasta .
+ln -s ../01_genome_assemblies/66b.fasta .
+ln -s ../01_genome_assemblies/Tu-10.fasta .
+
+### Serbia Xeu strains
+fasterq-dump SRR24958750 SRR24958751 SRR24958752 
+
+### Xeu Tu-10
+fasterq-dump SRR23352206
+
+### Xeu 66b
+fasterq-dump SRR4714703
+
+### Vietnam Xeu strains
+fasterq-dump SRR26670402 SRR26670404 SRR26670403 SRR26670400 SRR26670399
+
+### Canada Xeu strains
+fasterq-dump SRR16936518 SRR16936575 SRR16936578 SRR16936582 SRR16936540
