9edd6b3b5c44e4380ff8267c6069e0da91df6e8a
David Studholme <d.j.studholme@exeter.ac.uk>
2025-09-24 06:40:35 +0000

    Cleaning up

---
+++ 04-phylogenomics/01_all_xeu/04_phame/commands.sh
@@ -0,0 +1,18 @@
+### This takes a while to run, so best to do it in a screen session
+#screen
+
+### Already installed PhaME into a Conda environment
+conda activate phame_env
+
+conda list -n phame_env > phame_env_packages.txt
+conda env export > phame_env.yaml
+
+
+
+
+
+### Assumes that PhaME is already installed
+phame ./phame.ctl
+
+
+
