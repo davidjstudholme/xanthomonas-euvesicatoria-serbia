4a19d53ea0374a6b5b130b2a36dae4b6692d06a1
David Studholme <d.j.studholme@exeter.ac.uk>
2025-12-20 17:46:30 +0000

    Separated into three separate alignments

--- 11_pXCV183_clinker/commands.sh
+++ 11_pXCV183_clinker/commands.sh
@@ -8,7 +8,10 @@ conda install -c conda-forge clinker
 conda list -n bwa_env > clinker_env_packages.txt
 conda env export > clinker_env.yaml
 
-clinker -p pXCV183.clinker.html JAUALM020000025.1.gb  JAUALM020000026.1.gb  JAUALM020000047.1.gb  pXCV183.gb
+clinker -p pXCV183.versus.JAUALM020000025.clinker.html JAUALM020000025.1.gb pXCV183.gb
+clinker -p pXCV183.versus.JAUALM020000026.clinker.html JAUALM020000026.1.gb pXCV183.gb
+clinker -p pXCV183.versus.JAUALM020000047.clinker.html JAUALM020000047.1.gb pXCV183.gb
+
 
 
 
