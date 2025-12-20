32c552dcad4b48b208ed3bc374936ba9f1239e02
David Studholme <d.j.studholme@exeter.ac.uk>
2025-12-20 20:21:18 +0000

    Trimmed down partial gbk sequence

--- 11_pXCV183_clinker/commands.sh
+++ 11_pXCV183_clinker/commands.sh
@@ -1,5 +1,9 @@
 
-python3 extract_gbk.py ssrS pepQ
+python3 extract_gbk.py xopAH pilM
+
+
+python3 extract_gbk.py WP_011345768.1 WP_011345576.1
+
 
 conda create -n clinker_env
 conda activate clinker_env
@@ -8,7 +12,7 @@ conda install -c conda-forge clinker
 conda list -n bwa_env > clinker_env_packages.txt
 conda env export > clinker_env.yaml
 
-clinker -p pXCV183.versus.JAUALM020000025.clinker.html JAUALM020000025.1.gb pXCV183.gb
+clinker -p pXCV183.versus.JAUALM020000025.clinker.html JAUALM020000025.1.gb pXCV183.xopH_to_pilM.gbk
 clinker -p pXCV183.versus.JAUALM020000026.clinker.html JAUALM020000026.1.gb pXCV183.gb
 clinker -p pXCV183.versus.JAUALM020000047.clinker.html JAUALM020000047.1.gb pXCV183.gb
 
