a3e0f9ec258ef471f57239aceee9e437f0629e91
David Studholme <d.j.studholme@exeter.ac.uk>
2025-12-21 10:05:31 +0000

    Refined the Clinker alignments

--- 11_pXCV183_clinker/commands.sh
+++ 11_pXCV183_clinker/commands.sh
@@ -1,4 +1,3 @@
-
 conda create -n clinker_env
 conda activate clinker_env
 conda install -c conda-forge clinker
@@ -7,21 +6,14 @@ conda list -n bwa_env > clinker_env_packages.txt
 conda env export > clinker_env.yaml
 
 rm *.gbk
-
 python3 extract_gbk.py WP_100101058.1 WP_152667176.1
-
 python3 extract_gbk.py WP_011345768.1 WP_011345711.1
-
 python3 extract_gbk.py MFB9033557.1 MFB9033484.1
-
 python3 extract_gbk.py MFB9033574.1 MFB9033634.1
+python3 extract_gbk.py WP_011345622.1 WP_049756400.1
 
 rm *.html
-
-clinker -p pXCV183.versus.JAUALM020000025.clinker.2.html JAUALM020000025.1.MFB9033557.1_to_MFB9033484.1.gbk pXCV183.WP_100101058.1_to_WP_152667176.1.gbk
-
-clinker -p pXCV183.versus.JAUALM020000026.clinker.3.html JAUALM020000026.1.MFB9033574.1_to_MFB9033634.1.gbk pXCV183.WP_011345768.1_to_WP_011345711.1.gbk
-
-clinker -p pXCV183.versus.JAUALM020000047.clinker.5.html JAUALM020000047.1.gb pXCV183.gb
-
+clinker -p 01.pXCV183.versus.JAUALM020000025.clinker.html JAUALM020000025.1.MFB9033557.1_to_MFB9033484.1.gbk pXCV183.WP_100101058.1_to_WP_152667176.1.gbk
+clinker -p 02.pXCV183.versus.JAUALM020000026.clinker.html JAUALM020000026.1.MFB9033574.1_to_MFB9033634.1.gbk pXCV183.WP_011345768.1_to_WP_011345711.1.gbk
+clinker -p 03.pXCV183.versus.JAUALM020000047.clinker.html JAUALM020000047.1.gb pXCV183.WP_011345622.1_to_WP_049756400.1.gbk
 
