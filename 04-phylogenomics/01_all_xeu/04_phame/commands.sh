5d0564a94a5422648c0daef0d2b36c7de3cc3994
David Studholme <d.j.studholme@exeter.ac.uk>
2025-12-30 11:09:35 +0000

    Ran PhaME with IQ-tree

--- 04-phylogenomics/01_all_xeu/04_phame/commands.sh
+++ 04-phylogenomics/01_all_xeu/04_phame/commands.sh
@@ -7,12 +7,8 @@ conda activate phame_env
 conda list -n phame_env > phame_env_packages.txt
 conda env export > phame_env.yaml
 
-
-
-
-
 ### Assumes that PhaME is already installed
 phame ./phame.ctl
-
-
+phame ./phame.IQtree.ctl
+phame ./phame.RAxML.ctl
 
